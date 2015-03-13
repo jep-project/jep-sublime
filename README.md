5b0b74e39624b5226347cf2398b5a21b10585cf1
Martin Thiede <martin.thiede@esrlabs.com>
2015-03-13 18:17:45 +0100

    use jep-python to find .jep config files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# jep-sublime
-JEP Sublime Plugin
+# JEP Sublime Plugin
+
+This is the JEP frontend plugin for sublime.
+
+## Installation
+
+Clone jep-sublime and jep-python into your Sublime package directory.
+
--- jep.py
+++ jep.py
@@ -1,8 +1,19 @@
 import sublime, sublime_plugin
+import sys
+from os.path import realpath, dirname, join
+
+sys.path.append(join(dirname(realpath(__file__)), "..", "jep-python"))
+from jep.config import ServiceConfigProvider 
 
 class JEPAutocomplete(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
     	print("pos:    "+str(locations[0]))
     	print("prefix: '"+prefix+"'")
     	print("file    " + view.file_name())
-    	return [["jeptest1", "jeptest1"], ["jeptest2", "jeptest2"]] 
+    	provider = ServiceConfigProvider()
+    	sc = provider.provide_for(view.file_name())
+    	if sc:
+    		print("command: "+sc.command)
+    	else:
+    		print("no JEP config")
+    	return [["jeptest1\thinter", "jeptest1"], ["jeptest2", "jeptest2"]]
