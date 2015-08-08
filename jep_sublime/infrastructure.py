60e871722521586f84e8891ea9d8fc352e3e95df
Mike Pagel <mike@mpagel.de>
2015-08-08 14:34:36 +0200

    Preparation to more completion invocation to handler class.

--- jep_sublime/infrastructure.py
+++ jep_sublime/infrastructure.py
@@ -29,19 +29,19 @@ class BackendAdapter(BackendListener):
     """
 
     def __init__(self, content_tracker=None, auto_completer=None, error_annotator=None):
-        self.frontend = Frontend([self])
+        self._frontend = Frontend([self])
         #: Map from connection to supported views.
-        self.connection_views_map = {}
-        self.file_connection_map = {}
-        self.next_token_id = 0
-        self.completion_response = None
+        self._connection_views_map = {}
+        self._file_connection_map = {}
+        self._next_token_id = 0
+        self._completion_response = None
 
         self.content_tracker = content_tracker or ContentTracker()
         self.auto_completer = auto_completer or Autocompleter(self)
         self.error_annotator = error_annotator or ErrorAnnotator(self)
 
     def connect(self, view):
-        self._get_connection_for_view(view)
+        self._get_or_create_connection_for_view(view)
         self.content_tracker.start_change_tracking(view)
 
     def disconnect(self, view):
@@ -53,42 +53,41 @@ class BackendAdapter(BackendListener):
         self.content_tracker.mark_content_modified(view)
 
     def on_completion_response(self, response, context):
-        self.completion_response = response
+        # TODO Remove this after synchronous call logic has been moved to library:
+        self._completion_response = response
 
     def request_completion(self, view, file, data, pos):
-        con = self._get_connection_for_view(view)
+        con = self.get_connection_for_view(view)
         if con and con.state is State.Connected:
-            token = str(self.next_token_id)
-            self.next_token_id += 1
-            con.send_message(ContentSync(file=file, data=data))
+            token = str(self._next_token_id)
+            self._next_token_id += 1
+            self._completion_response = None
             con.send_message(CompletionRequest(token=token, file=file, pos=pos))
-            self.completion_response = None
             for i in range(0, 50):
                 con.run(datetime.timedelta(milliseconds=FRONTEND_POLL_DURATION_MS))
-                if self.completion_response:
+                if self._completion_response:
                     break
-            return self.completion_response
+            return self._completion_response
         else:
-            _logger.warning("Completion request cannot be served, no connection.")
+            _logger.warning('Completion request cannot be served, no connection for file %s.' % file)
             return None
 
-    def _get_connection_for_view(self, view):
+    def _get_or_create_connection_for_view(self, view):
         # do we already have a connection for this view?
-        filename = view.file_name()
-        con = self.file_connection_map.get(filename)
-
-        # create one by filename if not:
+        con = self.get_connection_for_view(view)
         if not con:
-            _logger.debug('Creating new connection for view %s.' % view.file_name())
-            con = self.frontend.get_connection(filename)
+            # try to create one:
+            filename = view.file_name()
+            _logger.debug('Creating new connection for file %s.' % filename)
+            con = self._frontend.get_connection(filename)
             if con:
-                self.file_connection_map[filename] = con
+                self._file_connection_map[filename] = con
 
                 # maybe the connection is already up as it was used by another file:
-                views = self.connection_views_map.get(con)
+                views = self._connection_views_map.get(con)
                 if not views:
                     views = []
-                    self.connection_views_map[con] = views
+                    self._connection_views_map[con] = views
 
                 views.append(view)
             else:
@@ -99,16 +98,22 @@ class BackendAdapter(BackendListener):
 
         return con
 
+    def get_connection_for_view(self, view):
+        """Public API to get an exiting connection for a view or None."""
+        filename = view.file_name()
+        con = self._file_connection_map.get(filename)
+        return con
+
     def _release_connection_for_view(self, view):
         """Releases the connection for the given view and returns the number of views left using this connection."""
         num_views_left = 0
 
-        _logger.debug('Number of connections: %d' % len(self.connection_views_map))
-        _logger.debug('Number of files:       %d' % len(self.file_connection_map))
+        _logger.debug('Number of connections: %d' % len(self._connection_views_map))
+        _logger.debug('Number of files:       %d' % len(self._file_connection_map))
         filename = view.file_name()
-        con = self.file_connection_map.pop(filename, None)
+        con = self._file_connection_map.pop(filename, None)
         if con:
-            views = self.connection_views_map[con]
+            views = self._connection_views_map[con]
             views.remove(view)
             num_views_left = len(views)
             if not num_views_left:
@@ -131,13 +136,13 @@ class BackendAdapter(BackendListener):
         view.set_status(STATUS_CATEGORY, STATUS_FORMAT % status)
 
     def run(self):
-        for con in self.connection_views_map.keys():
+        for con in self._connection_views_map.keys():
             previous_state = con.state
 
             con.run(datetime.timedelta(milliseconds=FRONTEND_POLL_DURATION_MS))
             new_state = con.state
 
-            for view in self.connection_views_map[con]:
+            for view in self._connection_views_map[con]:
 
                 # show status changes:
                 if new_state is not previous_state:
